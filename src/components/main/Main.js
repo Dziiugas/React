1e7b523782abb258195a8f65c7f90f36bd3e8d62
Dziiugas <145454289+Dziiugas@users.noreply.github.com>
2023-12-07 11:21:33 +0200

    Add files via upload

---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+import Article from "../article/Article"
+const Main = ()=>{
+    return(
+        <main>
+        <h1>naujienos</h1>
+        <p>pradejom mokytis react</p>
+        <Article/>
+        <Article/>
+        <Article/>
+        </main>
+    )
+   
+}
+export default Main
